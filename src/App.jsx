17cbbe6738feb68b04e45061a431de248f564837
meganw22 <mwalford22@gmail.com>
2025-02-03 21:28:00 +0000

    Experimenting with tailwind flex positions

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 const App = () => {
   return (
-    <h1 className=" text-4xl text-fuchsia-600 font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className=" text-4xl font-bold underline">Hello world!</h1>
+      <div className="grid grid-cols-3 grid-rows-3 gap-3 h-200">
+        <button className="grow order-1">#1 top left corner</button>
+        <button className="grow order-3">#3 top right corner</button>
+        <button className="grow order-4">#4 middle left</button>
+        <button className="grow order-5">#5 centre position</button>
+        <button className="grow order-6">#6 middle right</button>
+        <button className="grow order-8">#8 bottom middle</button>
+        <button className="grow order-9">#9 bottom right</button>
+        <button className="grow order-7">#7 bottom left</button>
+        <button className="grow order-2">#2 middle top</button>
+      </div>
+    </div>
   );
 };
 
